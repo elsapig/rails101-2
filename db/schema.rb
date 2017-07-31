e4b4c99d8b1a974bb17009a09254d5128ba7bf77
dreamrobot <chingchih.wu@gmail.com>
2017-08-01 03:18:57 +0800

    implement post create function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731143138) do
+ActiveRecord::Schema.define(version: 20170731190931) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20170731143138) do
   create_table "posts", force: :cascade do |t|
     t.text     "content"
     t.integer  "group_id"
-    t.integer  "use_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
